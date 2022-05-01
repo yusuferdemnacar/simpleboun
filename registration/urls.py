2a6b754a748be2c755b6d0dc4d951c7a95408046
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-05-01 18:46:26 +0300

    Added student addition page.

--- registration/urls.py
+++ registration/urls.py
@@ -4,7 +4,26 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #login urls
+    path('studentIndex', views.studentIndex, name="studentIndex"),
+    path('managerIndex', views.managerIndex, name="managerIndex"),
+    path('instructorIndex', views.instructorIndex, name="instructorIndex"),
     path('studentLogin', views.studentLogin, name="studentLogin"),
     path('managerLogin', views.managerLogin, name="managerLogin"),
     path('instructorLogin', views.instructorLogin, name="instructorLogin"),
+    #home page urls
+    path('studentHome', views.studentHome, name="studentHome"),
+    path('managerHome', views.managerHome, name="managerHome"),
+    path('instructorHome', views.instructorHome, name="instructorHome"),
+    #manager urls
+    path('managerHome/addStudentPage', views.addStudentPage, name="managerHome/addStudentPage"),
+    path('managerHome/addInstructorPage', views.addInstructorPage, name="managerHome/addInstructorPage"),
+    path('managerHome/deleteStudentPage', views.deleteStudentPage, name="managerHome/deleteStudentPage"),
+    
+    path('managerHome/addStudent', views.addStudent, name="managerHome/addStudent"),
+    path('managerHome/addInstructor', views.addInstructor, name="managerHome/addInstructor"),
+    path('managerHome/deleteStudent', views.deleteStudent, name="managerHome/deleteStudent"),
+    
+    
+    path('toy', views.toy, name="toy")
 ]
--- registration/views.py
+++ registration/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from .forms import *
 from .db_utils import run_statement
 
-# Create your views here.
+#First page
 
 def index(req):
     
@@ -13,7 +13,9 @@ def index(req):
     
     return render(req,'index.html')
     
-def managerLogin(req):
+#Login views
+    
+def managerIndex(req):
 
     isFailed=req.GET.get("fail",False) #Check the value of the GET parameter "fail"
     
@@ -21,7 +23,7 @@ def managerLogin(req):
 
     return render(req,'managerLogin.html',{"login_form":loginForm,"action_fail":isFailed})
     
-def studentLogin(req):
+def studentIndex(req):
     
     isFailed=req.GET.get("fail",False) #Check the value of the GET parameter "fail"
     
@@ -29,10 +31,136 @@ def studentLogin(req):
 
     return render(req,'studentLogin.html',{"login_form":loginForm,"action_fail":isFailed})
 
-def instructorLogin(req):
+def instructorIndex(req):
     
     isFailed=req.GET.get("fail",False) #Check the value of the GET parameter "fail"
     
     loginForm=UserLoginForm() #Use Django Form object to create a blank form for the HTML page
 
     return render(req,'instructorLogin.html',{"login_form":loginForm,"action_fail":isFailed})
+    
+def managerLogin(req):
+
+    #Retrieve data from the request body
+    username=req.POST["username"]
+    password=req.POST["password"]
+
+    result=run_statement(f"SELECT * FROM Database_manager WHERE username='{username}' and password='{password}';") #Run the query in DB
+
+    if result: #If a result is retrieved
+        req.session["username"]=username #Record username into the current session
+        return HttpResponseRedirect('../registration/managerHome') #Redirect user to home page
+    else:
+        return HttpResponseRedirect('../registration/managerIndex?fail=true')
+    
+def studentLogin(req):
+    
+    #Retrieve data from the request body
+    username=req.POST["username"]
+    password=req.POST["password"]
+
+    result=run_statement(f"SELECT * FROM Student WHERE username='{username}' and password='{password}';") #Run the query in DB
+
+    if result: #If a result is retrieved
+        req.session["username"]=username #Record username into the current session
+        return HttpResponseRedirect('../registration/toy') #Redirect user to home page
+    else:
+        return HttpResponseRedirect('../registration/studentIndex?fail=true')
+
+def instructorLogin(req):
+    
+    #Retrieve data from the request body
+    username=req.POST["username"]
+    password=req.POST["password"]
+
+    result=run_statement(f"SELECT * FROM Instructor WHERE username='{username}' and password='{password}';") #Run the query in DB
+
+    if result: #If a result is retrieved
+        req.session["username"]=username #Record username into the current session
+        return HttpResponseRedirect('../registration/toy') #Redirect user to home page
+    else:
+        return HttpResponseRedirect('../registration/instructorIndex?fail=true')
+        
+#Home pages
+    
+def managerHome(req):
+
+    username=req.session["username"]
+
+    return render(req,'managerHome.html', {"username":username})
+    
+def studentHome(req):
+    
+    #Retrieve data from the request body
+    username=req.POST["username"]
+    password=req.POST["password"]
+
+    result=run_statement(f"SELECT * FROM Student WHERE username='{username}' and password='{password}';") #Run the query in DB
+
+    if result: #If a result is retrieved
+        req.session["username"]=username #Record username into the current session
+        return HttpResponseRedirect('../registration/toy') #Redirect user to home page
+    else:
+        return HttpResponseRedirect('../registration/studentIndex?fail=true')
+
+def instructorHome(req):
+    
+    #Retrieve data from the request body
+    username=req.POST["username"]
+    password=req.POST["password"]
+
+    result=run_statement(f"SELECT * FROM Instructor WHERE username='{username}' and password='{password}';") #Run the query in DB
+
+    if result: #If a result is retrieved
+        req.session["username"]=username #Record username into the current session
+        return HttpResponseRedirect('../registration/toy') #Redirect user to home page
+    else:
+        return HttpResponseRedirect('../registration/instructorIndex?fail=true')
+
+#Manager operations
+
+def addStudentPage(req):
+
+    state=req.GET.get("state", "begin")
+    username=req.session["username"]
+
+    return render(req, "addStudent.html", {"state":state, "username":username})
+    
+def addStudent(req):
+
+    logged_user=req.session["username"]
+    username=req.POST["username"]
+    password=req.POST["password"]
+    student_id=req.POST["student_id"]
+    name=req.POST["name"]
+    surname=req.POST["surname"]
+    email=req.POST["email"]
+    department_id=req.POST["department_id"]
+    
+    try:
+        run_statement(f"INSERT INTO Student VALUES('{username}', {student_id}, '{department_id}', '{password}', '{name}', '{surname}', '{email}')")
+        return HttpResponseRedirect("../managerHome/addStudentPage?state=success")
+    except Exception as e:
+        print(str(e))
+        return HttpResponseRedirect('../managerHome/addStudentPage?state=fail')
+
+    pass
+    
+def addInstructorPage(req):
+
+    return render(req, "toy.html")
+    
+def addInstructor(req):
+
+    pass
+
+def deleteStudentPage(req):
+
+    return render(req, "toy.html")
+    
+def deleteStudent(req):
+
+    pass
+
+def toy(req):
+    return render(req, "toy.html")
